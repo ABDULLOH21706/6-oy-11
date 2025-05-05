f818c69072770a6be0f3798d718bd113fb03b0e0
mrzayev_abdulloh <abdullohmirzayev841@gmail.com>
2025-05-05 17:47:57 +0500

    comment

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../index.css";
 import { Button } from "antd";
 import { Heart, ShoppingBag } from "lucide-react";
@@ -9,6 +9,12 @@ import katalog from "../../assets/img/katalog.png";
 import profile from "../../assets/img/profile.png";
 
 const Navbar = () => {
+  // Initialize state for view
+  const [view, setView] = useState("month");
+
+  const handleViewChange = () => {
+    setView((prev) => (prev === "month" ? "year" : "month"));
+  };
   return (
     <header className="flex flex-col items-center justify-center w-[100%]">
       <div className="flex items-center justify-between w-[80%] m-auto py-2">
@@ -51,6 +57,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
       <div className="flex items-center justify-between w-[80%] m-auto py-4 gap-[30px]">
         <div className="w-[270px] cursor-pointer">
           <img src={logo} alt="" draggable="false" />
@@ -63,17 +70,21 @@ const Navbar = () => {
             Katalog
           </button>
           <div className="flex items-center justify-center w-[100%]">
-            <input type="text" id="search" placeholder="Search" className="w-[80%] h-[45px] outline-none border-[1px] border-[#c9c9c9] p-[10px] rounded-[8px]" draggable="false" />
+            <input
+              type="text"
+              id="search"
+              placeholder="Search"
+              className="w-[80%] h-[45px] outline-none border-[1px] border-[#c9c9c9] p-[10px] rounded-[8px]"
+              draggable="false"
+            />
           </div>
         </div>
         <div className="flex items-center gap-8">
           <div className="flex items-center justify-center gap-2 cursor-pointer">
             <div className="w-[29px]">
-              <img src={profile} alt="" draggable='false'/>
+              <img src={profile} alt="" draggable="false" />
             </div>
-            <p className="text-[16px]">
-            Kirish
-            </p>
+            <p className="text-[16px]">Kirish</p>
           </div>
           <div className="flex items-center justify-center gap-2 cursor-pointer">
             <Heart className="text-[16px]" />
@@ -84,6 +95,19 @@ const Navbar = () => {
             Savat
           </div>
         </div>
+      </div>
+
+      <div className="flex items-center justify-center w-[80%] m-auto py-4 gap-[20px] flex-wrap">
+        <p>Hafta tovarlari</p>
+        <p>Elektronika</p>
+        <p>Maishiy texnika</p>
+        <p>Kiyim</p>
+        <p>Poyabzallar</p>
+        <p>Aksessuarlar</p>
+        <p>Goʻzallik va parvarish</p>
+        <p>Salomatlik</p>
+        <p>Uy-roʻzgʻor buyumlari</p>
+        <p>Qurilish va taʼmirlash</p>
       </div>
     </header>
   );
